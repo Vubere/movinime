8568c241e1f4c442e966cf59a5fbc56e119a6e0e
Vubere <victorubere@gmail.com>
2022-08-11 03:48:36 +0100

    first commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+//import "./Navbar.scss"
+
+export default function Navbar(){
+  return (
+    <header>
+      <div aria-label="h1" className="heading">MOVIE APP</div>
+      <div aria-label="h1"
+       className="heading anime">ANIME APP</div>
+    </header>
+  )
+}
