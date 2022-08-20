1dd586096c6058025f423c579377150ab985ba96
Vubere <victorubere@gmail.com>
2022-08-20 15:56:34 +0100

    added logo and search bar to nav bar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,32 @@
+import { useRef } from "react"
 
 
-export default function Navbar(){
+export default function Navbar() {
+  const pages = useRef<HTMLDivElement>(null)
   return (
     <header>
-      <div aria-label="h1" className="heading">MOVIE</div>
-      <div aria-label="h1"
-       className="heading anime">ANIME</div>
+      <div className="top">
+        <div className="left">
+          <span className="hamburger" onClick={()=>{
+            if(pages.current!==null){
+                pages.current.style.display =
+                  pages.current.style.display === 'flex'?'none':'flex';
+            }
+          }}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </span>
+          <div className="logo">MoviNime</div>
+        </div>
+        <input type="text" name="search" id="search" placeholder="search" />
+      </div>
+      <div className="pages" ref={pages}>
+        <div aria-label="h1" className="heading">MOVIE</div>
+        <span className="span"></span>
+        <div aria-label="h1"
+          className="heading anime">ANIME</div>
+      </div>
     </header>
   )
 }
